9ff1a8027e8673bf3a499c5c8c5465b099d50931
Alan <alanmendoza057@gmail.com>
2021-12-27 20:27:05 -0600

    working in Edit button

--- js/main.js
+++ js/main.js
@@ -12,16 +12,18 @@ let to_do = {};
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   if (input.value === "") {
-    
-    empty.classList.add('is_invalid')
-    empty.textContent = 'Introduce algo :]'
+    empty.classList.add("is_invalid");
+    empty.textContent = "Introduce algo :]";
     input.focus();
   } else {
     listItem = document.createElement("li");
-    listItem.textContent += input.value;
+    listItemText = document.createElement("p");
+    listItemText.textContent += input.value;
     ul.appendChild(listItem);
+    listItem.appendChild(listItemText);
 
     listItem.appendChild(deleteElement());
+    listItem.appendChild(editElement());
 
     input.value = "";
     input.focus();
@@ -34,15 +36,40 @@ const deleteElement = () => {
   buttonDelete = document.createElement("button");
   buttonDelete.textContent = "Delete";
 
-  buttonDelete.classList.add('button_delete')
+  buttonDelete.classList.add("button_delete");
   buttonDelete.addEventListener("click", (e) => {
     let li = e.target.parentElement;
     ul.removeChild(li);
-    let lis = document.querySelectorAll('li')
+    let lis = document.querySelectorAll("li");
     if (lis.length === 0) {
-    empty.style.display = "block";
+      empty.style.display = "block";
     }
-});
+  });
   //   retorno el boton
   return buttonDelete;
+};
+
+const editElement = () => {
+  buttonEdit = document.createElement("button");
+  buttonEdit.textContent = "Edit";
+
+  buttonEdit.addEventListener("click", (e) => {
+    let li_edit = e.target.parentElement;
+    let newInput = document.createElement("input");
+    let  p = li_edit.children[0]
+    p.style.display = "none";
+    
+    newInput.setAttribute("type", "text");
+    newInput.value = p.textContent;
+    li_edit.appendChild(newInput);
+
+    buttonEdit.textContent = "Save";
+
+    buttonEdit.addEventListener("click", () => {
+      listItemText.textContent = newInput.value;
+      listItemText.style.display = "block";
+      newInput.remove()
+    });
+  });
+  return buttonEdit;
 };
